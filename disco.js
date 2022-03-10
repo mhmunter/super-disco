329174c55656eace027afc3c94ea2699989d34d0
M Munter <mark.munter1@gmail.com>
2022-03-10 11:27:49 -0600

    added first time and fixed css

---
+++ disco.js
@@ -0,0 +1,5 @@
+var now = moment();
+
+// document.querySelector("#currentDay").innerText = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
